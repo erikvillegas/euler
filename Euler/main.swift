2e7dd9cfa403776c222662e5c4bd62c639b6ecb0
Erik Villegas <erikvillegas@Eriks-MacBook-Pro.local>
2017-06-23 18:44:19 -0700

    Initial Commit

---
+++ Euler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Euler
+//
+//  Created by Erik Villegas on 6/23/17.
+//  Copyright © 2017 Erik Villegas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
